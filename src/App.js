5efbd48c911ccac500a5041801c60d629985e6f5
Walid0222 <hqsstudio221@gmail.com>
2024-10-02 01:15:06 +0100

    update8TawkTo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';  // Tout importé ici
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';  
 import Header from './header/header';  
 import ProductList from './products/ProductList';  
 import ProductPage from './products/ProductPage';  
@@ -7,13 +7,24 @@ import Footer from './footer/footer';
 import './App.css';
 
 function AppContent() {
-  const location = useLocation();  // On utilise useLocation à l'intérieur d'un composant encapsulé dans Router
+  const location = useLocation(); 
+
+  useEffect(() => {
+    // Code d'intégration Tawk.to
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function() {
+      var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/66fc8eab4cbc4814f7e1c187/1i958qjgl'; // Votre code d'intégration
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }, []);
 
   return (
     <div className="App">
       <Header />
-      {/* Afficher la bannière seulement sur la page d'accueil */}
-      
       <Routes>
         <Route path="/" element={<ProductList />} />
         <Route path="/product/:productName" element={<ProductPage />} />
@@ -26,7 +37,6 @@ function AppContent() {
 function App() {
   return (
     <Router>
-      {/* Le contenu principal de l'application */}
       <AppContent />
     </Router>
   );
